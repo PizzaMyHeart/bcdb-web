0be8fcef8e6898829d120dc4e54a7778c472d896
PizzaMyHeart <ian_tan@hotmail.com>
2024-04-02 13:34:23 +0100

    order comments by id

--- app/article/[article_id]/page.tsx
+++ app/article/[article_id]/page.tsx
@@ -5,16 +5,16 @@ export default async function article({ params }) {
     const articles = await prisma.$queryRaw`WITH counts AS (SELECT article_id, COUNT(*) AS row_count FROM comments GROUP BY article_id) SELECT articles.*, counts.row_count AS comment_count FROM articles JOIN counts ON articles.id = counts.article_id WHERE articles.id=${article_id};`;
     //const comments = await prisma.comments.findMany({where: {article_id: article_id}})
     const comments = await prisma.$queryRaw`WITH RECURSIVE comment_tree AS (
-        SELECT id, guardian_id, parent_guardian_id, author_name, author_name AS replying_to, body, 0 AS depth, ARRAY[id] AS path
+        SELECT id, guardian_id, parent_guardian_id, author_name, author_name AS replying_to, body, 0 AS depth, date
         FROM comments
         WHERE article_id=${article_id}
         AND parent_guardian_id IS NULL
         UNION ALL
-        SELECT c.id, c.guardian_id, c.parent_guardian_id, c.author_name, ct.author_name AS replying_to, c.body, ct.depth + 1 AS depth, ct.path || ct.guardian_id
+        SELECT c.id, c.guardian_id, c.parent_guardian_id, c.author_name, ct.author_name AS replying_to, c.body, ct.depth + 1 AS depth, ct.date
         FROM comments c
         JOIN comment_tree ct ON c.parent_guardian_id = ct.guardian_id
         )
-        SELECT id, guardian_id, parent_guardian_id, author_name, replying_to, body, depth FROM comment_tree ORDER BY path;`;
+        SELECT id, guardian_id, parent_guardian_id, author_name, replying_to, body, depth, date FROM comment_tree ORDER BY id;`;
     function depthPad (depth: number) {
         // Pain
         switch (depth) {
