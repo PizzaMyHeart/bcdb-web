eca0ad21d45cbdc58ed765e7b371ed17d4f86e12
PizzaMyHeart <ian_tan@hotmail.com>
2024-04-02 13:29:50 +0100

    dynamically set comment padding based on depth

--- app/article/[article_id]/page.tsx
+++ app/article/[article_id]/page.tsx
@@ -16,19 +16,55 @@ export default async function article({ params }) {
         )
         SELECT id, guardian_id, parent_guardian_id, author_name, replying_to, body, depth FROM comment_tree ORDER BY path;`;
     function depthPad (depth: number) {
+        // Pain
         switch (depth) {
             case 0:
                 return "pl-0"
             case 1:
-                return "pl-[30px]"
+                return "pl-[1vw]"
             case 2:
-                return "pl-[60px]"
+                return "pl-[2vw]"
             case 3:
-                return "pl-[90px]"
+                return "pl-[3vw]"
             case 4:
-                return "pl-[120px]"
+                return "pl-[4vw]"
+            case 5:
+                return "pl-[5vw]"
+            case 6:
+                return "pl-[6vw]"
+            case 7:
+                return "pl-[7vw]"
+            case 8:
+                return "pl-[8vw]"
+            case 9:
+                return "pl-[9vw]"
+            case 10: 
+                return "pl-[10vw]"
+            case 11: 
+                return "pl-[11vw]"
+            case 12: 
+                return"pl-[12vw]"
+            case 13: 
+                return"pl-[13vw]"
+            case 14: 
+                return"pl-[14vw]"
+            case 15: 
+                return"pl-[15vw]"
+            case 16: 
+                return"pl-[16vw]"
+            case 17: 
+                return"pl-[17vw]"
+            case 18: 
+                return"pl-[18vw]"
+            case 19: 
+                return"pl-[19vw]"
+            case 20: 
+                return"pl-[20vw]"
         }
+ 
     }
+
+  
     return (
         <div>
             <h1>Article {articles[0].id}</h1>
@@ -47,6 +83,7 @@ export default async function article({ params }) {
             {comments.map(comment => {
                 return (
                     <div className={depthPad(comment.depth)} key={comment.id}>
+                        <p>{depthPad(comment.depth)} || {comment.depth}</p>
                         <p>{comment.guardian_id}</p>
                         <p>{comment.author_name} (replying to {comment.replying_to} {comment.parent_guardian_id})</p>
                         <div className="pl-4" dangerouslySetInnerHTML={{ __html: comment.body}}></div>
