556f48dadac7c06d87a44771c1fd022aeeb3a975
PizzaMyHeart <ian_tan@hotmail.com>
2024-05-05 11:09:41 +0100

    error subquery in FROM must have an alias -- tags page

--- app/tag/[tag_id]/page.tsx
+++ app/tag/[tag_id]/page.tsx
@@ -5,7 +5,7 @@ import { Badge } from "@/components/ui/badge"
 
 export default async function allArticlesWithTag({ params }) {
     const tag_id = parseInt(params.tag_id);
-    const articles = await prisma.$queryRaw`SELECT * FROM (SELECT articles.title, articles.is_closed_for_comments, articles.id AS article_id, articles.permalink AS article_permalink, articles.published_date, tags.name AS tag_name, tags.id AS tag_id, tags.permalink AS tag_permalink, articles.num_comments AS num_comments FROM articles_tags JOIN articles ON articles.id = articles_tags.article_id JOIN tags ON tags.id = articles_tags.tag_id) WHERE num_comments IS NOT NULL AND tag_id=${tag_id};`;
+    const articles = await prisma.$queryRaw`SELECT * FROM (SELECT articles.title, articles.is_closed_for_comments, articles.id AS article_id, articles.permalink AS article_permalink, articles.published_date, tags.name AS tag_name, tags.id AS tag_id, tags.permalink AS tag_permalink, articles.num_comments AS num_comments FROM articles_tags JOIN articles ON articles.id = articles_tags.article_id JOIN tags ON tags.id = articles_tags.tag_id) AS tagged WHERE num_comments IS NOT NULL AND tag_id=${tag_id};`;
    
     return (
         <div>
