ea60bb62ea3216ac03d3ba8a764aee71c8236935
PizzaMyHeart <ian_tan@hotmail.com>
2024-05-05 10:15:03 +0100

    updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,6 +9,12 @@ export default function About() {
             <p>
                 Alternatively, browse by <a href="/article/all">article title</a> or <a href="/tag/all">tag</a>.
             </p>
+            <h1 className="text-2xl my-8">Why?</h1>
+            <p>
+                Besides <a href="https://www.goodreads.com/" target="_blank">Goodreads</a> and <a href="https://www.reddit.com" target="_blank">Reddit</a>, 
+                I&apos;ve found the Guardian to be a useful source of book-related discussion (there are some gems amidst the unfortunate politically-charged mudslinging, like when I was hunting for some good historical fiction). The problem is not all their book-related pages are open to comments, 
+                and there isn&apos;t an easy way to search the text of the comments instead of the parent article.
+            </p>
             <h1 className="text-2xl my-8">Features</h1>
             <ul className="list-none list-inside">
                 <li>☑ Full-text search of comments</li>
@@ -26,8 +32,18 @@ export default function About() {
                 <li><a href="https://www.postgresql.org/" target="_blank">PostgreSQL</a> and <a href="https://www.meilisearch.com/" target="_blank">Meilisearch</a></li>
                 <li><a href="https://github.com/PizzaMyHeart/bcdb/blob/main/requirements.txt" target="_blank">Python</a> for data wrangling</li>
             </ul>
+            <h1 className="text-2xl my-8">Hardware</h1>
+            <p><a href="https://docs.hetzner.com/cloud/servers/overview/#pricing" target="_blank">Hetzner CX11 VPS</a> running Debian 12</p>
+            <ul className="list-disc list-inside">
+                <li>1 vCPU</li>
+                <li>2 GB RAM</li>
+                <li>20 GB SSD</li>
+                <li>+ 20 GB storage volume (for Meilisearch)</li>
+            </ul>
             <h1 className="text-2xl my-8">Source</h1>
-            <a href="https://github.com/PizzaMyHeart/bcdb-web" target="_blank">GitHub</a>
+            <a href="https://github.com/PizzaMyHeart/bcdb-web" target="_blank">GitHub</a> <sup>spaghetti warning🍝</sup>
+            <h1 className="text-2xl my-8">Who?</h1>
+            <a href="https://tachy.org" target="_blank">Me</a>
         </div>
     )
 }
